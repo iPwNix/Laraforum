3d991056c39bee7a501ece3e037441fc22734d3c
Erik Vlasblom <erik_vlasblom@hotmail.com>
2018-02-05 11:52:39 +0100

    First Push
    First Lesson

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -5,14 +5,14 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ThreadsTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function a_user_can_browse_threads()
     {
         $response = $this->get('/');
 
