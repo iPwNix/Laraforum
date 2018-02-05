4462c727ddc81c320f5baf4efbce2faa00e0404c
Erik Vlasblom <erik_vlasblom@hotmail.com>
2018-02-05 17:02:41 +0100

    Lesson #8

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/threads', 'ThreadsController@index')->name('threadIndex');
-Route::get('/threads/{thread}', 'ThreadsController@show');
-Route::post('/threads', 'ThreadsController@store');
+//Route::get('/threads', 'ThreadsController@index')->name('threadIndex');
+//Route::get('/threads/{thread}', 'ThreadsController@show');
+//Route::get('/threads/create', 'ThreadsController@create');
+//Route::post('/threads', 'ThreadsController@store');
+Route::resource('threads', 'ThreadsController');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,20 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    //https://gist.github.com/adamwathan/125847c7e3f16b88fa33a9f8b42333da
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->disableExceptionHandling();
+    }
 
     protected function signIn($user = null){
         $user = $user ?: create('App\User');
@@ -15,4 +24,26 @@ abstract class TestCase extends BaseTestCase
 
         return $this;
     }
+
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(\Exception $e) {}
+            public function render($request, \Exception $e) {
+                throw $e;
+            }
+        });
+    }
+
+
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+
+        return $this;
+    }
+
 }
