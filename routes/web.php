4b0e39f9da2746a2cd315e83b8501c18f7b60a2b
Erik Vlasblom <erik_vlasblom@hotmail.com>
2018-02-05 16:06:17 +0100

    Lesson #6

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/threads', 'ThreadsController@index')->name('threadIndex');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::post('/threads', 'ThreadsController@store');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
